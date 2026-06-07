4c3a4456bcc70a15f1296bd954ff23f146fea1cd
khalil-440 <khalilkhairullah01@gmail.com>
2026-06-07 07:29:38 +0000

    khalil love daffa

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pymysql
 
 def get_db():
     return pymysql.connect(
-        host="127.0.0.1",
+        host="100.76.221.96",
         user="tecadmin",
         password="tec123",
         database="monitoring_db",
