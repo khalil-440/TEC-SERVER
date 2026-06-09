280c7dbfa0ab6c1d5e4dd63381937548eea136fe
khalil-440 <khalilkhairullah01@gmail.com>
2026-06-09 03:04:33 +0000

    sukim

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+while true
+do
+
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 RAM=$(free | awk '/Mem:/ {printf("%.2f"), $3/$2 * 100}')
 DISK=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 SWAP=$(free | awk '/Swap:/ {if($2==0) print 0; else printf("%.2f"), $3/$2 * 100}')
 USERS=$(who | wc -l)
-
-echo "CPU=$CPU"
-echo "RAM=$RAM"
-echo "DISK=$DISK"
-echo "SWAP=$SWAP"
-echo "USERS=$USERS"
 
 mysql -u tecadmin -ptec123 monitoring_db -e "
 INSERT INTO monitoring_logs
@@ -18,3 +15,7 @@ INSERT INTO monitoring_logs
 VALUES
 ($CPU, $RAM, $DISK, $SWAP, $USERS);
 "
+
+sleep 5
+
+done
