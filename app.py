00e37118f1e1837ab0fc310a4c0fb1c8c1dc74d4
khalil-440 <khalilkhairullah01@gmail.com>
2026-06-09 03:50:14 +0000

    fix chart

--- app.py
+++ app.py
@@ -336,24 +336,6 @@ def dbtest():
 
     return "Database Connected"
 
-@app.route("/api/monitoring/history")
-def monitoring_history():
-
-    db = get_db()
-    cur = db.cursor()
-
-    cur.execute("""
-        SELECT cpu_usage, ram_usage, timestamp
-        FROM monitoring_logs
-        ORDER BY id DESC
-        LIMIT 20
-    """)
-
-    rows = cur.fetchall()
-
-    rows.reverse()
-
-    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run(
