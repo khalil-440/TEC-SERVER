3545b7ec7484e61c07a866cf935d49dafd590dad
khalil-440 <khalilkhairullah01@gmail.com>
2026-06-11 14:18:24 +0700

    psje

--- app.py
+++ app.py
@@ -733,6 +733,11 @@ def heartbeat():
 
     return jsonify({"success": True})
 
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
